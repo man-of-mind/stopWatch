dc440efaa36776f1a4f3c02d1a2e7f3e1285c884
Abiodun Ganiyu <timothyabiodun300@gmail.com>
2022-02-07 01:19:47 +0100

    done with html and css

---
+++ js/index.js
@@ -0,0 +1,14 @@
+var millisecond = 0;
+var second = 0;
+var minute = 0;
+var d = 0;
+var e = 0;
+var z = 0;
+var check = true;
+var clear = 0;
+
+var stopwatch = document.getElementById("display");
+stopwatch.innerHTML = "00:00:00";
+stopwatch.style.fontSize = "60px";
+stopwatch.style.textAlign = "center";
+stopwatch.style.paddingTop = "50px";
